95f8f7d333a9706be9f5ef7a3d3e26d0a6397cb2
omermishania <omer.mishania@gmail.com>
2023-06-29 14:24:34 +0300

    Removed startup script on master

--- main.tf
+++ main.tf
@@ -32,9 +32,6 @@ resource "google_compute_instance_template" "master-template" {
     subnetwork = google_compute_subnetwork.subnet.self_link
     access_config {}
   }
-  metadata_startup_script = <<-EOF
-    curl -sSL https://kurl.sh/b58a0f8 | sudo bash
-  EOF
   depends_on = [google_compute_network.network, google_compute_subnetwork.subnet]
 }
 
